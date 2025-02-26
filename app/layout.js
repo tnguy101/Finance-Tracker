ed83d37f1e810a1db450193df5e2c3fc0f32f4bc
tnguy101 <tnguy@ad.unc.edu>
2025-02-26 17:11:30 -0500

    Set up user authentication

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono, Inter} from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({subsets: ["latin"]})
 
@@ -11,18 +12,20 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className}`}>
-        <Header></Header>
-        <main className="min-h-screen">
-        {children}
-        </main>
-        <footer className="bg-blue-50 py-12">
-          <div className="container mx-auto px-4 text-center text-gray-600">
-            <p>Made by tnguy</p>
-          </div>
-        </footer>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className}`}>
+          <Header/>
+          <main className="min-h-screen">
+          {children}
+          </main>
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>Made by tnguy</p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <Button variant="destructive">Tyler Finance Tracker Test</Button>;
+  return <div className="mt-40">
+  <Button variant="destructive">Tyler Finance Tracker Test</Button>;
+  </div>
 }
